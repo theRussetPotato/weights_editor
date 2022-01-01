2ea2b9ae7af779dc1425951216c81e03a6724568
Jason Labbe <jasonlabbe@gmail.com>
2022-01-01 18:51:42 +0800

    Fixing PYTHONPATH for unit tests on git

--- scripts/weights_editor_tool/tests/base.py
+++ scripts/weights_editor_tool/tests/base.py
@@ -5,9 +5,14 @@ from unittest import TestCase
 
 # Add repository base path to system paths, so Maya can access your scripts.
 tests_path = os.path.dirname(os.path.realpath(__file__))
+
 base_path = tests_path.rsplit(os.sep, 1)[0]
 if base_path not in sys.path:
     sys.path.insert(0, base_path)
+
+root_path = base_path.rsplit(os.sep, 1)[0]
+if root_path not in sys.path:
+    sys.path.insert(0, root_path)
 
 # Initialize Maya - otherwise tests run before Maya is actually ready!
 import maya.standalone
